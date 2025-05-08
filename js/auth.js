9cf8d6468f72afee10fc3fd9688de2155f6df96b
Harrisson Zapata <117449695+Harrissonhz@users.noreply.github.com>
2025-05-07 20:42:26 -0500

    Delete js directory

--- js/auth.js
+++
@@ -1,7 +0,0 @@
-// Verificar si el usuario está logueado
-const isLoggedIn = localStorage.getItem('isLoggedIn');
-const userName = localStorage.getItem('userName');
-
-if (isLoggedIn === 'true' && userName) {
-    document.getElementById('userName').textContent = userName;
-} 
